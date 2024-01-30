d04c7de37b81e8e9250307333b1ed5fff58cdb21
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2024-01-29 20:57:24 -0500

    lab3-updates

--- groceries.js
+++ groceries.js
@@ -1,118 +1,136 @@
-	
-// Array of products, each product is an object with different fieldset
-// A set of ingredients should be added to products		 
-
 var products = [
 	{
-	  name: "Broccoli",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: false,
-	  price: 1.99
+		name: "  Broccoli",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: 1.99,
+		image: "styles/broccoli.png"
 	},
 	{
-	  name: "Bread",
-	  vegetarian: true,
-	  glutenFree: false,
-	  organic: false,
-	  price: 2.35
+		name: "  Bread",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 2.35,
+		image: "styles/bread.png"
 	},
 	{
-	  name: "Salmon",
-	  vegetarian: false,
-	  glutenFree: true,
-	  organic: true,
-	  price: 10.00
+		name: "  Salmon",
+		vegetarian: false,
+		glutenFree: true,
+		organic: true,
+		price: 10.00,
+		image: "styles/salmon.png"
 	},
 	{
-	  name: "Tomatoes",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: true,
-	  price: 3.50
+		name: "  Tomatoes",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 3.50,
+		image: "styles/tomatoes.png"
 	},
 	{
-	  name: "Chicken",
-	  vegetarian: false,
-	  glutenFree: true,
-	  organic: false,
-	  price: 8.50
+		name: "  Chicken Drumsticks",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 8.50,
+		image: "styles/chicken_drumsticks.png"
 	},
 	{
-	  name: "Quinoa",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: true,
-	  price: 5.99
+		name: "  Quinoa",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 5.99,
+		image: "styles/quinoa.png"
 	},
 	{
-	  name: "Milk",
-	  vegetarian: false,
-	  glutenFree: true,
-	  organic: false,
-	  price: 2.99
+		name: "  Milk",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 2.99,
+		image: "styles/milk.png"
 	},
 	{
-	  name: "Apples",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: true,
-	  price: 1.50
+		name: "  Apples",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 1.50,
+		image: "styles/apples.png"
 	},
 	{
-	  name: "Eggs",
-	  vegetarian: false,
-	  glutenFree: true,
-	  organic: false,
-	  price: 4.50
+		name: "  Eggs",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 4.50,
+		image: "styles/eggs.png"
 	},
 	{
-	  name: "Spinach",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: true,
-	  price: 2.75
-	},
-	// Additional products to meet the requirement
-	{
-	  name: "Pasta",
-	  vegetarian: true,
-	  glutenFree: false,
-	  organic: false,
-	  price: 3.25
+		name: "  Spinach",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 2.75,
+		image: "styles/spinach.png"
 	},
 	{
-	  name: "Bananas",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: false,
-	  price: 1.75
+		name: " Pasta",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 3.25,
+		image: "styles/pasta.png"
 	},
 	{
-	  name: "Beef",
-	  vegetarian: false,
-	  glutenFree: true,
-	  organic: false,
-	  price: 12.99
+		name: " Bananas",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: 1.75,
+		image: "styles/bananas.png"
 	},
 	{
-	  name: "Potatoes",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: false,
-	  price: 2.25
+		name: " Beef",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 12.99,
+		image: "styles/beef.png"
 	},
 	{
-	  name: "Orange Juice",
-	  vegetarian: true,
-	  glutenFree: true,
-	  organic: true,
-	  price: 4.99
+		name: "  Potatoes",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: 2.25,
+		image: "styles/potatoes.png"
 	},
-  ];
-  
-products.sort((a, b) => a.price - b.price);  
-	
+	{
+		name: "  Orange Juice",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 4.99,
+		image: "styles/orange_juice.png"
+	},
+	{
+		name: "  Chicken Breasts",
+		vegetarian: false,
+		glutenFree: true,
+		organic: true,
+		price: 8.99,
+		image: "styles/chicken_breasts.png"
+	},
+];
+
+products.sort((a, b) => a.price - b.price);
+
 
 
 // given restrictions provided, make a reduced list of products
--- main.js
+++ main.js
@@ -1,85 +1,180 @@
-
-// This function is called when any of the tab is clicked
-// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
-
 function openInfo(evt, tabName) {
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
-	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
-	// Show the current tab, and add an "active" class to the button that opened the tab
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
+function populateListProductChoices(slct1, slct2) {
+    var s1 = document.getElementById(slct1);
+    var s2 = document.getElementById(slct2);
 
+    s2.innerHTML = "";
+
+    var optionArray = restrictListProducts(products, s1.value);
+
+    var gridContainer = document.createElement("div");
+    gridContainer.className = "product-grid";
+
+    for (i = 0; i < optionArray.length; i++) {
+        var productName = optionArray[i];
+        var product = products.find((item) => item.name === productName);
+
+        var gridItem = document.createElement("div");
+        gridItem.className = "product-item";
+
+        var productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = productName;
+        gridItem.appendChild(productImage);
+
+        var infoContainer = document.createElement("div");
+        infoContainer.className = "info-container";
+
+        var label = document.createElement("label");
+        label.innerHTML = `<strong>${productName}</strong>`;
+
+        var price = document.createElement("p");
+        price.innerHTML = `$${product.price.toFixed(2)}`;
+
+        infoContainer.appendChild(label);
+        infoContainer.appendChild(price);
+
+        gridItem.appendChild(infoContainer);
+
+        var addButton = document.createElement("button");
+        addButton.type = "button";
+        addButton.innerHTML = "Add to Cart";
+        addButton.addEventListener("click", (function (name) {
+            return function () {
+                addToCart(name);
+            };
+        })(productName));
+
+        gridItem.appendChild(addButton);
+
+        gridContainer.appendChild(gridItem);
+    }
+
+    s2.appendChild(gridContainer);
+}
+
+var cart = [];
+
+function addToCart(productName) {
+    var existingProduct = cart.find(item => item.name === productName);
+
+    if (existingProduct) {
+        existingProduct.quantity += 1;
+        console.log(`Added 1 more ${productName} to cart. Total quantity: ${existingProduct.quantity}`);
+    } else {
+        cart.push({ name: productName, quantity: 1 });
+        console.log(`${productName} added to cart. Quantity: 1`);
+    }
+    updateCartDisplay();
+    updateProductBoxes();
+}
+
+function updateProductBoxes() {
+    var productItems = document.getElementsByClassName("product-item");
+
+    for (var i = 0; i < productItems.length; i++) {
+        var productName = productItems[i].querySelector("label").innerText;
+        var existingProduct = cart.find(item => item.name === productName);
+
+        if (existingProduct && existingProduct.quantity > 1) {
+            var quantityButton = document.createElement("button");
+            quantityButton.type = "button";
+            quantityButton.className = "quantity-circle";
+            quantityButton.innerHTML = existingProduct.quantity;
+            productItems[i].appendChild(quantityButton);
+        }
+    }
+}
+
+function removeFromCart(productName) {
+    var existingProductIndex = cart.findIndex(item => item.name === productName);
+
+    if (existingProductIndex !== -1) {
+        cart[existingProductIndex].quantity -= 1;
+
+        if (cart[existingProductIndex].quantity === 0) {
+            cart.splice(existingProductIndex, 1);
+            console.log(`${productName} removed from cart.`);
+        } else {
+            console.log(`Removed 1 ${productName} from cart. Total quantity: ${cart[existingProductIndex].quantity}`);
+        }
+        updateCartDisplay();
+    }
+}
+
+function updateCartDisplay() {
+    var cartDisplay = document.getElementById('displayCart');
+    var totalPriceDisplay = document.getElementById('totalPriceDisplay');
+    cartDisplay.innerHTML = "";
+
+    if (cart.length > 0) {
+        var para = document.createElement("p");
+
+        for (var i = 0; i < cart.length; i++) {
+            var productContainer = document.createElement("div");
+
+            var removeButton = document.createElement("button");
+            removeButton.type = "button";
+            removeButton.id = "removeButton";
+            removeButton.innerHTML = "X";
+            removeButton.addEventListener("click", (function (name) {
+                return function () {
+                    removeFromCart(name);
+                };
+            })(cart[i].name));
+
+            productContainer.appendChild(removeButton);
+
+            var cartItemText = cart[i].quantity > 1 ? `${cart[i].name} ${cart[i].quantity}x` : cart[i].name;
+            productContainer.appendChild(document.createTextNode(cartItemText));
+
+            para.appendChild(productContainer);
+            para.appendChild(document.createElement("br"));
+        }
+
+        cartDisplay.appendChild(para);
+        totalPriceDisplay.textContent = "$" + getTotalPrice(cart).toFixed(2);
+    } else {
+        cartDisplay.innerHTML = "Your cart is empty.";
+        totalPriceDisplay.textContent = "";
+    }
+}
+
+function getTotalPrice(cart) {
+    return cart.reduce((total, item) => {
+        var product = products.find((product) => product.name === item.name);
+        return total + product.price * item.quantity;
+    }, 0);
 }
 
 
-	
-// generate a checkbox list from a list of products
-// it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct1, slct2) {
-	var s1 = document.getElementById(slct1);
-	var s2 = document.getElementById(slct2);
-  
-	s2.innerHTML = "";
-  
-	var optionArray = restrictListProducts(products, s1.value);
 
-	for (i = 0; i < optionArray.length; i++) {
-	  var productName = optionArray[i];
+function sortProducts(sortBy) {
+    if (sortBy === "price") {
+        products.sort((a, b) => a.price - b.price);
+    } else if (sortBy === "name") {
+        products.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    populateListProductChoices('dietSelect', 'displayProduct');
+}
 
-	  var product = products.find((item) => item.name === productName);
-	  var checkbox = document.createElement("input");
-	  checkbox.type = "checkbox";
-	  checkbox.name = "product";
-	  checkbox.value = productName;
-	  s2.appendChild(checkbox);
-  
-	  var label = document.createElement('label')
-	  label.htmlFor = productName;
-	  label.appendChild(document.createTextNode(`${productName} - $${product.price.toFixed(2)}`));
-	  s2.appendChild(label);
-  	  s2.appendChild(document.createElement("br"));
-	}
-  }
-	
-// This function is called when the "Add selected items to cart" button in clicked
-// The purpose is to build the HTML to be displayed (a Paragraph) 
-// We build a paragraph to contain the list of selected items, and the total price
 
-function selectedItems(){
-	
-	var ele = document.getElementsByName("product");
-	var chosenProducts = [];
-	
-	var c = document.getElementById('displayCart');
-	c.innerHTML = "";
-	
-	// build list of selected item
-	var para = document.createElement("P");
-	para.innerHTML = "You selected : ";
-	para.appendChild(document.createElement("br"));
-	for (i = 0; i < ele.length; i++) { 
-		if (ele[i].checked) {
-			para.appendChild(document.createTextNode(ele[i].value));
-			para.appendChild(document.createElement("br"));
-			chosenProducts.push(ele[i].value);
-		}
-	}
-		
-	// add paragraph and total price
-	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
-		
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
 }
 
